86c8e55d571d57f99e94c0c9a8eecbcadd4dc3c5
Nayakv46 <bartoszjaniak04@gmail.com>
2026-07-08 18:15:19 +0200

    routes usage for organizations and mm_users_organizations

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import organizationsRouter from "./organizations";
+import mmUsersOrganizationsRouter from "./mmUsersOrganizations";
 
 const router = Router();
+
+router.use("/organizations", organizationsRouter);
+router.use("/mm_users_organizations", mmUsersOrganizationsRouter);
 
 export default router;
