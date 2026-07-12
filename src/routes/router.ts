8adb1a5f1ca27da1fa3355f27130aa4710969a2d
Nayakv46 <bartoszjaniak04@gmail.com>
2026-07-12 13:50:14 +0200

    Auth router integration

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import organizationsRouter from "./organizations";
 import mmUsersOrganizationsRouter from "./mmUsersOrganizations";
+import authRouter from "./auth";
 
 const router = Router();
 
+router.use("/auth", authRouter);
 router.use("/organizations", organizationsRouter);
 router.use("/mm_users_organizations", mmUsersOrganizationsRouter);
 
